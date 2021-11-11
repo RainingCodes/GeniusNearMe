cd26b524abf4362e33b95f87c844684cff730e39
sohyun <shparkso162@gmail.com>
2021-11-12 01:35:31 +0900

    test3

--- src/main/java/persistence/dao/MatchingDAO.java
+++ src/main/java/persistence/dao/MatchingDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 import service.dto.MatchingDTO;
 public interface MatchingDAO {
 	//매징 리스트 반환  userId에 따른
+	
 	public List<MatchingDTO> getMatchingListByUserId(int userId);
 	//매칭상세 보기
 	public MatchingDTO getMatchingByMatchingId(int matchingId);
