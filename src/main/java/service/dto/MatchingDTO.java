692196af946483f48c019c8a7f52f92c8f79ce5f
sohyun <shparkso162@gmail.com>
2021-11-11 20:54:42 +0900

    update matchingDTO

--- src/main/java/service/dto/MatchingDTO.java
+++ src/main/java/service/dto/MatchingDTO.java
@@ -5,6 +5,27 @@ public class MatchingDTO {
 	
 	private int matchingId = -1;
 	private int matchingState = -1;
+	private int talentId = -1;
+	private int groupId = -1;
+	private int userId = -1;
+	public int getTalentId() {
+		return talentId;
+	}
+	public void setTalentId(int talentId) {
+		this.talentId = talentId;
+	}
+	public int getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 	public int getMatchingId() {
 		return matchingId;
 	}
