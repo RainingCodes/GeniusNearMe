dc7e4b3381176929a3d0a614a9d53028f56fc7f5
leemj <20190988@dongduk.ac.kr>
2021-11-11 22:06:44 +0900

    groupDAO representative

--- src/main/java/persistence/dao/GroupDAO.java
+++ src/main/java/persistence/dao/GroupDAO.java
@@ -7,6 +7,7 @@ public interface GroupDAO {
 	public int insertGroup(int groupId);
 	public int[] getGroupMembers(int groupId);
 	public int insertGroupMember(int userId);
+	public int getRepresentative(int groupId, int userId);
 	public int setRepresentative(int groupId, int userId);
 	public List<GroupDTO> getGroupList();
 	public int deleteGroupMember(int userId);
