fff8764cd6f1063d89cfe9eabe672fb8627dfeb9
sohyun <shparkso162@gmail.com>
2021-11-11 20:42:09 +0900

    matchingDTO

---
+++ src/main/java/service/dto/MatchingDTO.java
@@ -0,0 +1,22 @@
+package service.dto;
+
+public class MatchingDTO {
+//¸ÅÄª dto
+	
+	private int matchingId = -1;
+	private int matchingState = -1;
+	public int getMatchingId() {
+		return matchingId;
+	}
+	public void setMatchingId(int matchingId) {
+		this.matchingId = matchingId;
+	}
+	public int getMatchingState() {
+		return matchingState;
+	}
+	public void setMatchingState(int matchingState) {
+		this.matchingState = matchingState;
+	}
+		
+
+}
