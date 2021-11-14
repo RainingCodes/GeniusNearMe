e957279d45aa584234695596bc0a3641afa86ad7
sohyun <shparkso162@gmail.com>
2021-11-14 11:00:21 +0900

    ff

--- src/main/java/controller/talent/RegisterTalentController.java
+++ src/main/java/controller/talent/RegisterTalentController.java
@@ -1,5 +1,17 @@
 package controller.talent;
 
-public class RegisterTalentController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+
+public class RegisterTalentController implements Controller{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
 
 }
