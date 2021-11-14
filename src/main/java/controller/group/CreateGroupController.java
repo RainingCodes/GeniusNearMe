67577e96e3031a7220034f73dcafda92d48e85a9
leemj <20190988@dongduk.ac.kr>
2021-11-14 16:12:04 +0900

    group_dto

--- src/main/java/controller/group/CreateGroupController.java
+++ src/main/java/controller/group/CreateGroupController.java
@@ -4,12 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
+import service.dto.GroupDTO;
 
 public class CreateGroupController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
+		GroupDTO group = new GroupDTO();
 		return null;
 	}
 
