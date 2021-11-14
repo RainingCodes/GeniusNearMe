7b516da33322b151d89f08be68184065a5a73157
leemj <20190988@dongduk.ac.kr>
2021-11-14 15:17:30 +0900

    implements

--- src/main/java/controller/group/CreateGroupController.java
+++ src/main/java/controller/group/CreateGroupController.java
@@ -1,5 +1,16 @@
 package controller.group;
 
-public class CreateGroupController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+
+public class CreateGroupController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
