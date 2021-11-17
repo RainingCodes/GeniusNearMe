fd19ff1a984fca2ba4c23e2ec810385a31071186
20190937 <77391428+20190937@users.noreply.github.com>
2021-11-17 20:39:38 +0900

    f

--- src/main/java/controller/user/ViewMemberController.java
+++ src/main/java/controller/user/ViewMemberController.java
@@ -14,6 +14,7 @@ import service.MemberServiceImpl;
 
 public class ViewMemberController implements Controller {
 	private static final Logger log = LoggerFactory.getLogger(ViewMemberController.class);
+	
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {			
     	
