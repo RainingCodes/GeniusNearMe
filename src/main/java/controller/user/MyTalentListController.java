f39b0f64313478ff679a125ef3d8b05405b51b40
sohyun <shparkso162@gmail.com>
2021-11-27 21:12:15 +0900

    myTalentList.jsp, controller 추가 및 수정

--- src/main/java/controller/user/MyTalentListController.java
+++ src/main/java/controller/user/MyTalentListController.java
@@ -1,16 +1,44 @@
 package controller.user;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import controller.Controller;
+import service.MemberService;
+import service.MemberServiceImpl;
+import service.TalentService;
+import service.TalentServiceImpl;
+import service.dto.TalentDTO;
 
 public class MyTalentListController implements Controller{
-
+	private static final Logger log = LoggerFactory.getLogger(MyTalentListController.class);
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		// 로그인 여부 확인
+    	if (!UserSessionUtils.hasLogined(request.getSession())) {
+    		System.out.println("not Logined");
+            return "redirect:/member/login/form";		// login form 요청으로 redirect
+        }
+    	
+    	MemberService manager = new MemberServiceImpl();
+    	String email = UserSessionUtils.getLoginUserId(request.getSession());
+		
+		log.debug("my talent List User : {}", email);
+		
+		int userId = manager.getuserIdByEmail(email);
+		
+		TalentService talentservice = new TalentServiceImpl();
+		List<TalentDTO> talentList = talentservice.getTalentByWriter(userId);
+		
+		request.setAttribute("talentList", talentList);
+		
+		return "/member/myTalentList.jsp";
 	}
 
 }
---
+++ src/main/webapp/member/myTalentList.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%! int i = 1; %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>내가 작성한 재능 게시글</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" href="../css/talent.css" type="text/css">
+</head>
+<body>
+<%@ include file="../main/head.jsp"  %>
+	<div class="content">
+		<c:forEach var="talent" items="${talentList}">
+			<div class="post">
+				<div id="imgSection1">
+					<img id="thumbnail" src="../img/loopy/img<%=i++ %>.jpg">
+				</div>
+				<p>
+					<a href="<c:url value='/talent/view'>
+						<c:param name='talentId' value='${talent.talentId}'/></c:url>">
+						<strong><c:out value="${talent.title}"/></strong>
+					</a>					
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<c:out value="${talent.writtenDate}"/>
+				</p>
+				<p></p>
+				<p></p>
+				<p><c:out value="${talent.content}"/></p>
+			</div>
+		</c:forEach>
+	</div>
+</body>
+</html>
