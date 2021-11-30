f8ee14e41c6bf88f0bc31970b57b1555ca0f0c2e
sohyun <shparkso162@gmail.com>
2021-11-30 23:18:28 +0900

    review 관련 컨트롤러 중간 커밋

---
+++ src/main/java/controller/talent/RegisterReviewController.java
@@ -0,0 +1,54 @@
+package controller.talent;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import controller.user.UserSessionUtils;
+import service.MemberService;
+import service.MemberServiceImpl;
+import service.ReviewService;
+import service.ReviewServiceImpl;
+import service.dto.ReviewDTO;
+
+public class RegisterReviewController implements Controller{
+	private static final Logger log = LoggerFactory.getLogger(RegisterReviewController.class);
+	
+	long miliseconds = System.currentTimeMillis();
+    Date current = new Date(miliseconds);
+    String src = null;
+    
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		int talentId = Integer.parseInt(request.getParameter("talentId"));
+		
+		String email = UserSessionUtils.getLoginUserId(request.getSession());
+		MemberService memberService = new MemberServiceImpl();
+		int userId = memberService.getuserIdByEmail(email);
+		
+		ReviewDTO review = new ReviewDTO(
+				current,
+				0,
+				userId,
+				talentId,
+				request.getParameter("content")
+		);
+		
+		log.debug("RegisterReviewController Request : {}", review);
+		
+		ReviewService service = new ReviewServiceImpl();
+		int key = service.insertReview(review);
+		
+		System.out.println("리뷰 생성 키: "+ key);
+		
+		src = "/talent/view?talentId=" +talentId;
+		
+		return src;
+	}
+
+}
---
+++ src/main/java/controller/talent/ViewReviewController.java
@@ -0,0 +1,31 @@
+package controller.talent;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import service.ReviewService;
+import service.ReviewServiceImpl;
+import service.dto.ReviewDTO;
+
+public class ViewReviewController implements Controller{
+	private static final Logger log = LoggerFactory.getLogger(ViewReviewController.class);
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		int talentId = Integer.parseInt(request.getParameter("talentId"));
+		
+		ReviewService reviewService = new ReviewServiceImpl();
+		List<ReviewDTO> reviewList = reviewService.getReviewListByTalent(talentId);
+		
+		request.setAttribute("reviewList", reviewList);
+		
+		return "";
+	}
+	
+}
