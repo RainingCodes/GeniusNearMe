523e068fb6f8ba67529b491af4ea66fe491694a3
leemj <20190988@dongduk.ac.kr>
2021-12-01 22:25:50 +0900

    where 추가

--- src/main/java/persistence/dao/impl/MyMatchingDAOImpl.java
+++ src/main/java/persistence/dao/impl/MyMatchingDAOImpl.java
@@ -51,7 +51,7 @@ public class MyMatchingDAOImpl implements MyMatchingDAO {
 	
 	public List<MyMatchingDTO> getReceiveMyMatchingListByUserId(int userId) {
 		String searchQuery = query + "FROM MATCHING, TALENT "+
-				"MATCHING.TALENTID = TALENT.TALENTID AND TALENT.WRITERID = ? ";
+				"WHERE MATCHING.TALENTID = TALENT.TALENTID AND TALENT.WRITERID = ? ";
 		
 		jdbcUtil.setSqlAndParameters(searchQuery, new Object[] { userId, userId });
 		
