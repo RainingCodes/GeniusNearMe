c40c6aa0fce3418f49e57c9bc342a3fd90be76c3
sumin <sumin00716@naver.com>
2021-12-18 22:52:37 +0900

    commentMapper.java, commetDAO.java

--- src/main/java/model/mapper/CommentMapper.java
+++ src/main/java/model/mapper/CommentMapper.java
@@ -2,7 +2,9 @@ package model.mapper;
 import model.Comment;
 
 public interface CommentMapper {
+	
 	Comment getCommentByUserId(int userId);
+	Comment getCommentByCommentId(int commentId);
 	public int insertComment(Comment comment);
 	public int updateComment(Comment comment);
 	public int deleteComment(int commentId);	
