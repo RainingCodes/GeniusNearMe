655b6a8d807762136ac8699cf1b6ea7577a16d1c
20190937 <77391428+20190937@users.noreply.github.com>
2021-12-18 21:54:32 +0900

    talentTest

--- src/main/java/model/TalentTest.java
+++ src/main/java/model/TalentTest.java
@@ -1,5 +1,39 @@
 package model;
 
 public class TalentTest {
-
+	private int userId;
+	private String resultCategory;
+	private String resultType;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getResultCategory() {
+		return resultCategory;
+	}
+	public void setResultCategory(String resultCategory) {
+		this.resultCategory = resultCategory;
+	}
+	public String getResultType() {
+		return resultType;
+	}
+	public void setResultType(String resultType) {
+		this.resultType = resultType;
+	}
+	
+	public TalentTest() {
+		this.resultCategory = null;
+		this.resultType = null;
+		this.userId = -1;
+	}
+	
+	public TalentTest(String resultCategory, String resultType, int userId) {
+		super();
+		this.resultCategory = resultCategory;
+		this.resultType = resultType;
+		this.userId = userId;
+	}
 }
--- src/main/java/model/mapper/TalentTestMapper.java
+++ src/main/java/model/mapper/TalentTestMapper.java
@@ -1,5 +1,9 @@
 package model.mapper;
+import model.TalentTest;
 
-public class TalentTestMapper {
-
+public interface TalentTestMapper {
+	TalentTest getTalentTestByUserId(int userId);
+	public int insertTalentTest(TalentTest t);   
+	public int updateTalentTest(TalentTest t);
+	public int deleteTalentTest(int userId);   
 }
